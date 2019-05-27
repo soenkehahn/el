e2c69006798e106af8417221bf504840af411f3d
Sönke Hahn <soenkehahn@gmail.com>
2019-05-26 20:56:13 -0400

    wip

--- El.hs
+++ El.hs
@@ -1,2 +1,12 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Language.Haskell.TH
+import System.Environment
+
 main :: IO ()
 main = putStrLn "\8211"
+
+foo :: ()
+foo = $(do
+  runIO (getEnv "LANGUAGE" >>= print)
+  return $ TupE [])
